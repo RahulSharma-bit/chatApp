cd993a859be225af60b67bcf42c743546bf4f08b
Rahul Sharama <ravigeneralstore161@gmail.com>
2024-06-21 07:25:59 +0530

    sending message to firestore

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,3 +1,5 @@
+import 'package:chat_app/widgets/chat_message.dart';
+import 'package:chat_app/widgets/new_message.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 // import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -8,23 +10,22 @@ class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Chat App'),
-        backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
-        actions: [
-          IconButton(
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              icon: const Icon(Icons.exit_to_app))
-        ],
-      ),
-      body: const Center(
-        child: Text(
-          'Logged in',
-          style: TextStyle(),
+        appBar: AppBar(
+          title: const Text('Flutter Chat App'),
+          backgroundColor: Theme.of(context).colorScheme.secondaryContainer,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                icon: const Icon(Icons.exit_to_app))
+          ],
         ),
-      ),
-    );
+        body: const Column(
+          children: [
+            ChatMessage(),
+            NewMessage(),
+          ],
+        ));
   }
 }
